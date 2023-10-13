c0f4445b7222e55cfc40ec2f8f77ff1471c3ccc7
Abdo-Mos <aabbddoo.sshh@gmail.com>
2023-10-12 23:13:00 -0400

    feat: user service Azure container URL

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import requests
 
 app = Flask(__name__)
 
-URL_REQ = "http://127.0.0.1:5000/user/"
+# URL_REQ = "http://127.0.0.1:5000/user/"
+URL_REQ = "https://userservicecontainer.wonderfulocean-aa423ae1.canadacentral.azurecontainerapps.io"
 
 posts = [
     {'id': 1, 'user_id': 1, 'post': 'Hello, world!'},
